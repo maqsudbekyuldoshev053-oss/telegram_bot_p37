ee45a9a64c5db0ff8c38e68eaa9d4b0c02806062
maqsudbek <maqsudbekyuldoshev053@gmail.com>
2026-03-01 19:17:01 +0500

    first_project

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
 from pydantic import Field
 from pydantic_settings import BaseSettings
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
 
 class Settings(BaseSettings):
+    TELEGRAM_BOT_TOKEN: str = Field()
     POSTGRES_USER: str = Field()
     POSTGRES_PASSWORD: str = Field()
     POSTGRES_HOST: str = Field()
@@ -13,20 +12,18 @@ class Settings(BaseSettings):
 
     @property
     def postgresql_url(self) -> str:
-        return (f"postgresql://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@"
-                f"{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DATABASE}")
+        return (f"postgresql://{self.POSTGRES_USER}:"
+                f"{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:"
+                f"{self.POSTGRES_PORT}/{self.POSTGRES_DATABASE}")
 
     @property
     def async_postgresql_url(self) -> str:
-        return (f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@"
-                f"{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DATABASE}")
+        return (f"postgresql+asyncpg://{self.POSTGRES_USER}:"
+                f"{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:"
+                f"{self.POSTGRES_PORT}/{self.POSTGRES_DATABASE}")
 
     class Config:
-        env_file = ".env"
+        env_file = '.env'
 
 
 settings = Settings()
-
-engine = create_engine(settings.postgresql_url, echo=True)
-
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
